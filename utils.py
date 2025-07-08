45b22b773f4789b11412c41ad9fa63f60464d998
theSreekari <sreekarigattu198@gmail.com>
2025-07-08 18:06:23 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
+# utils.py
 
+import PyPDF2
+import docx
+
+def extract_text_from_pdf(uploaded_file):
+    reader = PyPDF2.PdfReader(uploaded_file)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+def extract_text_from_docx(uploaded_file):
+    doc = docx.Document(uploaded_file)
+    return "\n".join([para.text for para in doc.paragraphs])
